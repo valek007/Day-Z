ed2e8e50eefeed0adc69803daf4fb8bf2d3d7172
Valek007 <tecleando.java@gmail.com>
2020-07-09 23:48:30 +0200

    Second Thread created and checked

--- src/Game.java
+++ src/Game.java
@@ -1,7 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class Game extends Canvas{
+public class Game extends Canvas implements Runnable{
 
     private static final long serialVersionUID = 1L;
     private static final int WIDTH = 800;
@@ -9,6 +9,7 @@ public class Game extends Canvas{
     private static final String GAMENAME = "Day-Z";
     private static JFrame gameFrame;
     private static Thread thread;
+    private static volatile boolean working;
 
     private Game() {
         setPreferredSize(new Dimension(WIDTH,HEIGTH));
@@ -23,8 +24,41 @@ public class Game extends Canvas{
         gameFrame.setVisible(true);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         Game myGame = new Game();
+        myGame.initialize();
+        Thread.sleep(1000);
+        myGame.stop();
+
+    }
+
+    private synchronized void initialize(){
+
+        working = true;
+        thread = new Thread(this, "Graphics");
+        thread.start();
+    }
+
+    private synchronized void stop() {
+
+        working = false;
+
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Thread is stopped");
+    }
+
+    @Override
+    public void run() {
+
+        while(working) {
+            System.out.println("Thread activated");
+        }
+
+
     }
 }
